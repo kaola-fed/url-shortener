ba0a0d3b2b87c011272ed7752a493e1a279fc797
Cody Chan <int64ago@gmail.com>
2017-04-29 17:34:10 +0800

    feat: add /shorten api

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+import sys, urllib, requests
+from flask import Flask, request
+app = Flask(__name__)
+
+if len(sys.argv) < 2:
+    sys.exit()
+
+USERNAME = sys.argv[1]
+BASE_URL = 'https://163.lu/'
+
+@app.route('/')
+def index():
+    return 'URL Shortener'
+
+@app.route('/shorten', methods = ['POST'])
+def shorten():
+    url = request.form['url']
+    url = urllib.unquote(url.encode('utf-8'))
+    params = {
+        'user': USERNAME,
+        'url': urllib.quote(url, safe = '~@#$&()*!+=:;,.?/\'')
+    }
+    res = requests.post('%sshorten/' % BASE_URL, params).json()
+
+    return BASE_URL + res['hash']
+
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0')
